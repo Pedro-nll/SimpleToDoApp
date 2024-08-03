1abdc888df30290da1eaeb0c588b1414dd77dc6a
Pedro-nll <pedronll@outlook.com>
2024-08-03 14:13:20 -0300

    chore: Refactor RemindersList component to handle error cases and improve code readability

--- Frontend/src/components/RemindersList.jsx
+++ Frontend/src/components/RemindersList.jsx
@@ -2,6 +2,10 @@ import React from 'react';
 
 // Function to group reminders by date
 const groupRemindersByDate = (reminders) => {
+  if (!Array.isArray(reminders)) {
+    throw new TypeError('Reminders is not an array');
+  }
+
   return reminders.reduce((groups, reminder) => {
     const dateParts = reminder.date.split('T')[0].split('-');
     const date = new Date(dateParts[0], dateParts[1] - 1, dateParts[2]);
@@ -18,6 +22,9 @@ const groupRemindersByDate = (reminders) => {
 
 // Function to sort dates
 const sortDates = (dates) => {
+  if (!dates){
+    return {}
+  }
   return dates.sort((a, b) => {
     const dateA = a.split('/').reverse().join('');
     const dateB = b.split('/').reverse().join('');
@@ -26,16 +33,22 @@ const sortDates = (dates) => {
 };
 
 const ReminderList = ({ reminders, onDeleteReminder, deletingReminderId, addingReminderId }) => {
-  const groupedReminders = groupRemindersByDate(reminders);
-  const sortedDates = sortDates(Object.keys(groupedReminders));
+  var sortedDates
+  var groupedReminders
+  try {
+    groupedReminders = groupRemindersByDate(reminders);
+    sortedDates = sortDates(Object.keys(groupedReminders));
+  } catch (error) {
+    console.log(error.message)
+  }
 
   return (
     <div className="reminder-list">
       <h2>Lista de lembretes</h2>
-      {sortedDates.map((date) => (
+      {sortedDates && sortedDates.map((date) => (
         <div key={date} className="reminder-group">
           <h3>{date}</h3>
-          {groupedReminders[date].map((reminder) => (
+          {groupedReminders && groupedReminders[date].map((reminder) => (
             <div
               key={reminder.id}
               className={`reminder ${deletingReminderId === reminder.id ? 'deleting' : ''} ${addingReminderId === reminder.id ? 'adding' : ''}`}
